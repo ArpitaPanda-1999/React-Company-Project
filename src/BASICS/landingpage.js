d5e383fc929410021fe00f47c3d0dc7f246e9104
ArpitaPanda-1999 <142828327+ArpitaPanda-1999@users.noreply.github.com>
2024-04-18 18:11:16 +0000

    change

--- src/BASICS/landingpage.js
+++ src/BASICS/landingpage.js
@@ -10,6 +10,12 @@ const Landing = () => {
             setClient(data.clientdetails);
         })
     }
+
+    useEffect(() => {
+        getClient();
+    }, [])
+
+
     return(
         <div>
             <nav className="navbar bg-body-tertiary p-0">
@@ -73,10 +79,31 @@ const Landing = () => {
                                 <a className="nav-link active text-light" href="#">Contact Us</a>
                             </li>
                         </ul>
-                        <form className="d-flex" role="search">
-                            <input className="form-control me-2 text-bg-success border-0" type="search" aria-label="Search"/>
-                            <button className="btn btn-success" type="submit"><i className="fa fa-search"></i></button>
-                        </form>
+                        <div className="d-flex" role="search">
+                        {/* <!-- Button trigger modal --> */}
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            Launch demo modal
+                            </button>
+
+                            {/* <!-- Modal --> */}
+                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    ...
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                </div>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </nav>
